996ac38e095ac772529112b716e70961643f7632
gurpreetbenipal <preett222@gmail.com>
2016-01-29 14:25:13 -0500

    changed registeration point of helloLabel

--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -23,6 +23,8 @@ function gameLoop(event:createjs.Event):void{           // All animations will d
 function main(){
     console.log("Game Started...");
     helloLabel = new createjs.Text("Hello World","60px Consolas", "#000000");
+    helloLabel.regX = helloLabel.getMeasuredWidth() * 0.5;
+    helloLabel.regY = helloLabel.getMeasuredHeight() * 0.5;
     stage.addChild(helloLabel);
 }
 
